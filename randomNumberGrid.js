87f013f68940c230286e9ff579795237af45ce7b
hesterhoyle <60700485+hesterhoyle@users.noreply.github.com>
2020-02-05 14:34:00 +0000

    Add files via upload

--- randomNumberGrid.js
+++ randomNumberGrid.js
@@ -1,14 +1,17 @@
+// Aimed to make a grid that shows numbers 1-9, without any repeating numbers but random layout.
+// Also wanted to add up the lines (diagonal directions, as well as vertical and horizontal directions)
+
 function shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
-  
-    // While there remain elements to shuffle...
+    
+    // while there's remaining elements to shuffle
     while (0 !== currentIndex) {
   
-      // Pick a remaining element...
+     // pick a remaining element
       randomIndex = Math.floor(Math.random() * currentIndex);
       currentIndex -= 1;
   
-      // And swap it with the current element.
+      // swaps it with a current element
       temporaryValue = array[currentIndex];
       array[currentIndex] = array[randomIndex];
       array[randomIndex] = temporaryValue;
@@ -17,15 +20,17 @@ function shuffle(array) {
     return array;
   }
   
+  // with the array and shuffle
   var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
   arr = shuffle(arr);;
 
 
-
+// added a function to add the 3 numbers together
 let r1 = (num1, num2, num3) => {
 return num1 + num2 + num3
 }
 
+//Grid and numbers display below
 console.log((r1(arr[0], arr[4], arr[8]))
 + "  " + (r1(arr[0], arr[3], arr[6]))
 + "  " + (r1(arr[1], arr[4], arr[7]))
